8758ef53a6c04051f0c0dfa2c0c56bfc9c50b5ed
rajkumar94 <rajkmr.rkp@gmail.com>
2018-08-28 18:40:37 +0530

    fixed webhook verification

--- middlewares/verifyWebhooks.js
+++ middlewares/verifyWebhooks.js
@@ -1,39 +1,22 @@
 var crypto = require('crypto');
 
-function verifyShopifyHook(req, accessToken='') {
-    // console.log(req.body);
-    var shopify_api_shared_secret = '';
-    if(accessToken){
-        shopify_api_shared_secret = accessToken;
-    }else{
-        if(process.env.DEPOLOYMENT === 'production'){
-            shopify_api_shared_secret = process.env.PRODUCTION_SHOPIFY_SHARED_SECRET;
-        }else{
-            shopify_api_shared_secret = process.env.TEST_SHOPIFY_SHARED_SECRET;
-        }
-    }
-    
+function verifyShopifyHook(req) {
     var message = req.rawBody;
-    var digest = crypto.createHmac('SHA256', shopify_api_shared_secret)
+    var digest = crypto.createHmac('SHA256', SHOPIFY_SHARED_SECRET)
             .update(message)
-            .digest('base64');
-    // console.log(req);
-    // console.log('header = '+req.headers['x-shopify-hmac-sha256']);
-    // console.log('digest = '+digest);        
+            .digest('base64');    
     return digest === req.headers['x-shopify-hmac-sha256'];
   }
 
 
-function verifyShopifyWebhook(req, res, accessToken = '') {
-    if (verifyShopifyHook(req, accessToken)) {
-        res.writeHead(200);
+function verifyShopifyWebhook(req, res) {
+    if (verifyShopifyHook(req)) {
         console.log('verified');
-        res.end('Verified webhook');
+        res.status(200).send({});
         return true;
     } else {
-        res.writeHead(401);
         console.log('unverified');
-        res.end('Unverified webhook');
+        res.status(401).send({});
         return false;
     }
 }
